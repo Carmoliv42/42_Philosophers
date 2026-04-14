7ca7483fe280b41d00233e90cd8b0e26539df4e0
root <root@DESKTOP-NUC73FD.localdomain>
2026-04-14 22:00:52 +0100

    Update

--- routine.c
+++ routine.c
@@ -1,24 +1,24 @@
 #include "philo.h"
-
+ 
 void think(t_philo *philo)
 {
     print_action(philo, "is thinking");
     usleep(500);
 }
-
+ 
 void	sleep_philo(t_philo *philo)
 {
 	print_action(philo, "is sleeping");
 	smart_sleep(philo, philo->data->time_to_sleep);
 }
-
+ 
 void *philo_routine(void *arg)
 {
 	t_philo *philo = (t_philo *)arg;
-
+ 
 	if (philo->id % 2 == 0)
 		usleep(500);
-
+ 
 	while (1)
 	{
 		pthread_mutex_lock(&philo->data->death_lock);
@@ -28,7 +28,7 @@ void *philo_routine(void *arg)
 			break;
 		}
 		pthread_mutex_unlock(&philo->data->death_lock);
-
+ 
 		eat(philo);
 		sleep_philo(philo);
 		think(philo);
