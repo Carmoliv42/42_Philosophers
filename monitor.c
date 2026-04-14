54c40c0498971b1af182756a18ce660d1d2c4039
root <root@DESKTOP-NUC73FD.localdomain>
2026-04-14 21:24:21 +0100

    Update

--- monitor.c
+++ monitor.c
@@ -1,16 +1,17 @@
 #include "philo.h"
-
+ 
 static int check_philo(t_data *data, int i, int *full_count)
 {
-	long last;
-
+	long	last;
+	long	meals;
+ 
 	pthread_mutex_lock(&data->meal_lock);
 	last = data->philos[i].last_meal;
-	if (data->must_eat != -1
-		&& data->philos[i].meals_eaten >= data->must_eat)
+	meals = data->philos[i].meals_eaten;
+	if (data->must_eat != -1 && meals >= data->must_eat)
 		(*full_count)++;
 	pthread_mutex_unlock(&data->meal_lock);
-
+ 
 	if (get_time() - last > data->time_to_die)
 	{
 		pthread_mutex_lock(&data->death_lock);
@@ -28,12 +29,12 @@ static int check_philo(t_data *data, int i, int *full_count)
 	}
 	return (0);
 }
-
+ 
 static int monitor_philos(t_data *data)
 {
-	int i;
-	int full_count;
-
+	int	i;
+	int	full_count;
+ 
 	i = 0;
 	full_count = 0;
 	while (i < data->num_philos)
@@ -42,7 +43,7 @@ static int monitor_philos(t_data *data)
 			return (1);
 		i++;
 	}
-
+ 
 	if (data->must_eat != -1 && full_count == data->num_philos)
 	{
 		pthread_mutex_lock(&data->death_lock);
@@ -52,13 +53,13 @@ static int monitor_philos(t_data *data)
 	}
 	return (0);
 }
-
-void *monitor_routine(void *arg)
+ 
+void	*monitor_routine(void *arg)
 {
-	t_data *data;
-
+	t_data	*data;
+ 
 	data = (t_data *)arg;
-
+ 
 	while (1)
 	{
 		pthread_mutex_lock(&data->death_lock);
@@ -68,10 +69,10 @@ void *monitor_routine(void *arg)
 			break;
 		}
 		pthread_mutex_unlock(&data->death_lock);
-
+ 
 		if (monitor_philos(data))
 			return (NULL);
-
+ 
 		usleep(1000);
 	}
 	return (NULL);
