eceb748fcbaf0dc2d3c6ed0c1b1902f673ec55f8
root <root@DESKTOP-NUC73FD.localdomain>
2026-04-19 22:18:51 +0100

    Update

--- actions.c
+++ actions.c
@@ -1,57 +1,64 @@
 #include "philo.h"
-
+ 
 void take_forks(t_philo *philo, pthread_mutex_t **first, pthread_mutex_t **second)
 {
-    if (philo->left_fork < philo->right_fork)
-    {
-        *first = philo->left_fork;
-        *second = philo->right_fork;
-    }
-    else
-    {
-        *first = philo->right_fork;
-        *second = philo->left_fork;
-    }
-
-    pthread_mutex_lock(*first);
-    print_action(philo, "has taken a fork");
-
-    pthread_mutex_lock(*second);
-    print_action(philo, "has taken a fork");
+	if (philo->left_fork < philo->right_fork)
+	{
+		*first = philo->left_fork;
+		*second = philo->right_fork;
+	}
+	else
+	{
+		*first = philo->right_fork;
+		*second = philo->left_fork;
+	}
+ 
+	pthread_mutex_lock(*first);
+	print_action(philo, "has taken a fork");
+ 
+	pthread_mutex_lock(*second);
+	print_action(philo, "has taken a fork");
 }
-
-
-static void do_eat(t_philo *philo,
+ 
+static void	do_eat(t_philo *philo,
 	pthread_mutex_t *first, pthread_mutex_t *second)
 {
 	print_action(philo, "is eating");
-
-	// 🔥 FIX CRÍTICO: atualiza aqui, NÃO antes
+ 
 	pthread_mutex_lock(&philo->data->meal_lock);
 	philo->last_meal = get_time();
 	philo->meals_eaten++;
 	pthread_mutex_unlock(&philo->data->meal_lock);
-
+ 
 	smart_sleep(philo, philo->data->time_to_eat);
-
+ 
 	pthread_mutex_unlock(second);
 	pthread_mutex_unlock(first);
 }
-
-void eat(t_philo *philo)
+ 
+void	eat(t_philo *philo)
 {
-	pthread_mutex_t *first;
-	pthread_mutex_t *second;
-
+	pthread_mutex_t	*first;
+	pthread_mutex_t	*second;
+ 
 	if (philo->data->num_philos == 1)
 	{
 		pthread_mutex_lock(philo->left_fork);
 		print_action(philo, "has taken a fork");
-		smart_sleep(philo, philo->data->time_to_die);
-		pthread_mutex_unlock(philo->left_fork);
-		return;
+		while (1)
+		{
+			pthread_mutex_lock(&philo->data->death_lock);
+			if (philo->data->simulation_end)
+			{
+				pthread_mutex_unlock(&philo->data->death_lock);
+				pthread_mutex_unlock(philo->left_fork);
+				return;
+			}
+			pthread_mutex_unlock(&philo->data->death_lock);
+			usleep(100);
+		}
 	}
-
+ 
 	pthread_mutex_lock(&philo->data->death_lock);
 	if (philo->data->simulation_end)
 	{
@@ -59,7 +66,7 @@ void eat(t_philo *philo)
 		return;
 	}
 	pthread_mutex_unlock(&philo->data->death_lock);
-
+ 
 	take_forks(philo, &first, &second);
 	do_eat(philo, first, second);
 }
