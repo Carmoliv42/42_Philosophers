85277e202e2a62c1adee8a08a5772ae688dbd4ff
root <root@DESKTOP-NUC73FD.localdomain>
2026-04-21 21:36:18 +0100

    Update

--- init.c
+++ init.c
@@ -1,5 +1,5 @@
 #include "philo.h"
-
+ 
 int init_data(t_data *data, char **av)
 {
     data->num_philos = atoi(av[1]);
@@ -8,28 +8,28 @@ int init_data(t_data *data, char **av)
     data->time_to_sleep = atol(av[4]);
     data->must_eat = (av[5]) ? atoi(av[5]) : -1;
     data->simulation_end = 0;
-
+ 
     data->forks = malloc(sizeof(pthread_mutex_t) * data->num_philos);
     data->philos = malloc(sizeof(t_philo) * data->num_philos);
-
+ 
     if (!data->forks || !data->philos)
         return (1);
-
+ 
     pthread_mutex_init(&data->print_lock, NULL);
     pthread_mutex_init(&data->death_lock, NULL);
     pthread_mutex_init(&data->meal_lock, NULL);
-
+ 
     for (int i = 0; i < data->num_philos; i++)
         pthread_mutex_init(&data->forks[i], NULL);
-
+ 
     data->start_time = get_time();
     return (0);
 }
-
+ 
 int init_philos(t_data *data)
 {
     int i;
-
+ 
     i = 0;
     while (i < data->num_philos)
     {
