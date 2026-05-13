77a3790b25a26cc132d38302c8723b4afe96abd1
root <root@DESKTOP-NUC73FD.localdomain>
2026-05-13 11:18:55 +0100

    Update

--- philo.h
+++ philo.h
@@ -1,14 +1,14 @@
 #ifndef PHILO_H
 # define PHILO_H
-
+ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/time.h>
 #include <pthread.h>
-
+ 
 typedef struct s_data t_data;
-
+ 
 typedef struct s_philo
 {
     int				id;
@@ -19,7 +19,7 @@ typedef struct s_philo
 	pthread_mutex_t *right_fork;
 	t_data			*data;
 } t_philo;
-
+ 
 typedef struct s_data
 {
 	int				num_philos;
@@ -27,34 +27,34 @@ typedef struct s_data
 	long			time_to_eat;
 	long			time_to_sleep;
 	int				must_eat;
-
+ 
 	long			start_time;
 	int				simulation_end;
-
+ 
 	pthread_mutex_t	*forks;
 	pthread_mutex_t	print_lock;
 	pthread_mutex_t meal_lock;
 	pthread_mutex_t	death_lock;
-
+ 
 	t_philo			*philos;
 } t_data;
-
+ 
 /*utils*/
 long	get_time(void);
 void 	smart_sleep(t_philo *philo, long time);
 void	print_action(t_philo *philo, char *str);
-
+ 
 /*action*/
 void	eat(t_philo *philo);
-
+ 
 /*init*/
 int	init_data(t_data *data, char **av);
 int	init_philos(t_data *data);
-
+ 
 /*routine*/
 void	*philo_routine(void *arg);
-
+ 
 /*monitor*/
 void	*monitor_routine(void *arg);
-
+ 
 #endif
